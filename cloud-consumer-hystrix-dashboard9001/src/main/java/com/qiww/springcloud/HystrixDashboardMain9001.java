9422f50ec2b0ccd3b62d9ce2e73cd8989899cb58
qiww <qiww567@126.com>
2022-12-08 19:04:52 +0800

    Hystrix图形化Dashboard监控 localhost:9001/hystrix

---
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/qiww/springcloud/HystrixDashboardMain9001.java
@@ -0,0 +1,19 @@
+package com.qiww.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+
+/**
+ * @author wlq
+ * @version 1.0
+ * @Email: qiww567@126.com
+ * @date 2022/12/8 18:38
+ */
+@SpringBootApplication
+@EnableHystrixDashboard
+public class HystrixDashboardMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(HystrixDashboardMain9001.class, args);
+    }
+}
