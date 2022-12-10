4ad86e046634512239c8bd4cce99c5a7c0dffad3
qiww <qiww567@126.com>
2022-12-10 18:11:27 +0800

    SpringCloudAlibaba-nacos服务配置中心 test

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/qiww/springcloud/NacosConfigClientMain3377.java
@@ -0,0 +1,19 @@
+package com.qiww.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author wlq
+ * @version 1.0
+ * @Email: qiww567@126.com
+ * @date 2022/12/10 16:18
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class NacosConfigClientMain3377 {
+    public static void main(String[] args) {
+        SpringApplication.run(NacosConfigClientMain3377.class, args);
+    }
+}
---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/qiww/springcloud/controller/ConfigClientController.java
@@ -0,0 +1,25 @@
+package com.qiww.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wlq
+ * @version 1.0
+ * @Email: qiww567@126.com
+ * @date 2022/12/10 16:19
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
